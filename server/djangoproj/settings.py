92691d03313c9a8b3d7d250ffd4e54ee0a309703
EmilianoAnaya <gael.anaya@alumnos.udg.mx>
2025-07-10 21:28:01 -0400

    Working with workflows Python_2

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,10 +30,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://zgrupo935-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://zgrupo935-8000.theiadockernext-1-labs-prod-'
+    'theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://zgrupo935-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://zgrupo935-8000.theiadockernext-1-labs-prod-'
+    'theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
